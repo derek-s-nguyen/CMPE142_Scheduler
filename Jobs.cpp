da69c77c6fc3ab214ea8e1b6433b02d1881cab12
derek-s-nguyen <48035548+derek-s-nguyen@users.noreply.github.com>
2019-04-10 14:56:18 -0700

    Create Jobs.cpp

---
+++ Jobs.cpp
@@ -0,0 +1,29 @@
+#include "Jobs.h"
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+using namespace std;
+#include <iostream>
+Jobs::Jobs(){
+	arrival = 0, completion = 0, ID = 0, duration = 0;
+}
+
+int Jobs::getArrival(){ return arrival; }
+
+int Jobs::getCompletion(){ return completion; }
+
+int Jobs::getID(){ return ID; }
+
+int Jobs::getDuration(){ return duration; }
+
+istream &operator >>(istream &in_stream, Jobs &job){
+	in_stream >> job.ID;
+	in_stream >> job.arrival;
+	in_stream >> job.duration;
+	return in_stream;
+}
+ostream &operator <<(ostream &out_stream, const Jobs &job){
+	out_stream << job.ID << "\t" << job.arrival << "\t" << job.duration << "\n";
+	return out_stream;
+}
+Jobs::~Jobs(){}
