5fea8d081c7861a4eb461214eee4975912c3431c
derek-s-nguyen <48035548+derek-s-nguyen@users.noreply.github.com>
2019-04-11 00:47:21 -0700

    Update Jobs.cpp
    implementing all necessary mutator and accessor functions as well as overloaded operators

--- Jobs.cpp
+++ Jobs.cpp
@@ -4,18 +4,16 @@
 #include <fstream>
 using namespace std;
 #include <iostream>
-Jobs::Jobs(){
-	arrival = 0, completion = 0, ID = 0, duration = 0;
-}
-
+Jobs::Jobs(){ arrival = 0, completion = 0, ID = 0, duration = 0, startTime = 0, finishTime = 0, totalTimeElapsed = 0, responseTime = 0; }
 int Jobs::getArrival(){ return arrival; }
-
-int Jobs::getCompletion(){ return completion; }
-
 int Jobs::getID(){ return ID; }
-
 int Jobs::getDuration(){ return duration; }
-
+void Jobs::operator =(const Jobs &job)
+{
+	ID = job.ID;
+	arrival = job.arrival;
+	duration = job.duration;
+}
 istream &operator >>(istream &in_stream, Jobs &job){
 	in_stream >> job.ID;
 	in_stream >> job.arrival;
@@ -27,3 +25,11 @@ ostream &operator <<(ostream &out_stream, const Jobs &job){
 	return out_stream;
 }
 Jobs::~Jobs(){}
+int Jobs::getStartTime(){ return startTime; }
+int Jobs::getFinishTime(){ return finishTime; }
+int Jobs::getTotalTimeElapsed(){ return totalTimeElapsed; }
+int Jobs::getResponseTime(){ return responseTime; }
+void Jobs::setStartTime(int start){ startTime = start; }
+void Jobs::setFinishTime(int finish){ finishTime = finish; }
+void Jobs::setTotalTimeElapsed(int total){ totalTimeElapsed = total; }
+void Jobs::setResponseTime(int response){ responseTime = response; }
