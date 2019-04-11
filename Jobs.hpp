05b64751aba9a4d1f56debacb9998403bd80c5c1
derek-s-nguyen <48035548+derek-s-nguyen@users.noreply.github.com>
2019-04-11 13:32:08 -0700

    Update Jobs.hpp

--- Jobs.hpp
+++ Jobs.hpp
@@ -18,7 +18,6 @@ public:
 	friend istream &operator >>(istream &in_stream, Jobs &job);
 	friend ostream &operator <<(ostream &out_stream, const Jobs &job);
 	void operator =(const Jobs &job);
-	void outputForJobs(Jobs *jobsArry);
 	~Jobs();
 	//calculated values depending on scheduling algorithms:
 	int getStartTime();
