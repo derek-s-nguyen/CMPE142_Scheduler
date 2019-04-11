8947147a9967ffae1f6d89103bb62acdec01bc20
derek-s-nguyen <48035548+derek-s-nguyen@users.noreply.github.com>
2019-04-11 00:49:02 -0700

    completed implementing FIFO scheduling function
    completed implementing FIFO scheduling function

--- main.cpp
+++ main.cpp
@@ -7,6 +7,15 @@
 #include <ctime>
 #include "Jobs.hpp"
 using namespace std;
+/*
+need to output:
+start time, finish time, total time elapsed, response time
+*/
+void FIFO(Jobs *jobsArry, int numberOfJobs);
+void SJF(Jobs *jobsArry, int numberOfJobs);
+void BJF(Jobs *jobsArry, int numberOfJobs);
+void STCF(Jobs *jobsArry, int numberOfJobs);
+void RR(Jobs *jobsArry, int numberOfJobs);
 int findNumJobs();
 int main() {
 	int numberOfJobsFound = 0;
@@ -16,15 +25,65 @@ int main() {
 	numberOfJobsFound = findNumJobs();
 
 	Jobs *jobsArry = new Jobs[numberOfJobsFound];
+
 	for(int i = 0; i < numberOfJobsFound; i++)
 	{
 		in_stream >> jobsArry[i];
-		cout << jobsArry[i];
+//		cout << jobsArry[i];
 	}
+	FIFO(jobsArry, numberOfJobsFound);
 
 	return 0;
 }
+//*NOTE: this FIFO function also sorts the array of jobs in increasing order of arrival times
+void FIFO(Jobs *jobsArry, int numberOfJobs){//no preemption
+	int i, j, k, a, min;
+	Jobs temp;
+	int currentTimeStamp = 0;//time stamp starts at zero
 
+	//sorting the jobs array by increasing order of arrival times using selection sort (for simplicity)
+    for (i = 0; i < (numberOfJobs - 1); i++)
+    {
+        min = i;
+        for (j = i + 1; j < numberOfJobs; j++){
+        	if (jobsArry[j].getArrival() < jobsArry[min].getArrival()){
+        		min = j;
+        	}
+        }
+        temp = jobsArry[min];
+        jobsArry[min] = jobsArry[i];
+        jobsArry[i] = temp;
+    }
+    /*
+    need to output:
+    start time, finish time, total time elapsed, response time
+    */
+    for(k = 0; k < numberOfJobs; k++){
+    	if(jobsArry[k].getArrival() > currentTimeStamp)//if there are no jobs to schedule
+    	{
+    		currentTimeStamp = jobsArry[k].getArrival();//adjust the current time stamp to skip to the next job's arrival time
+    	}
+    	jobsArry[k].setStartTime(currentTimeStamp);//start time is the current time stamp because FIFO (array is sorted)
+    	jobsArry[k].setFinishTime((jobsArry[k].getDuration() + currentTimeStamp));
+    	currentTimeStamp = jobsArry[k].getFinishTime();
+    }
+    //printing job results for FIFO
+    for(a = 0; a < numberOfJobs; a++){
+    	cout << "Job ID " << jobsArry[a].getID() << ":\n"
+    			<< "\tStart Time: " << jobsArry[a].getStartTime() << endl
+				<< "\tFinish Time: " << jobsArry[a].getFinishTime() << endl
+				<< "\tTotal Time Elapsed: " << (jobsArry[a].getFinishTime() - jobsArry[a].getArrival()) << endl
+				<< "\tResponse Time: " << (jobsArry[a].getStartTime() - jobsArry[a].getArrival()) << endl;
+    }
+}
+void SJF(Jobs *jobsArry, int numberOfJobs){//no preemption
+
+}
+void BJF(Jobs *jobsArry, int numberOfJobs){//no preemption
+
+}
+void STCF(Jobs *jobsArry, int numberOfJobs){}
+void RR(Jobs *jobsArry, int numberOfJobs){}
 int findNumJobs()
 {
 	int numberOfJobsFound = 0, counter = 0;
